00a0dfa1bdc33ec7a138edee075344c596500a5d
Jayden1Joe <jojojo69988@naver.com>
2025-06-26 16:28:55 +0900

    Feat(input '3' => showTotal, input '4' => quit the program)

--- bin/shopping_malll.dart
+++ bin/shopping_malll.dart
@@ -20,7 +20,7 @@ class ShoppingMall {
   }
 
   showTotal() {
-    print('총 금액: $total원');
+    print('장바구니에 $total원 어치를 담으셨네요 !');
   }
 }
 
@@ -47,40 +47,48 @@ void main(List<String> arguments) {
 
   ShoppingMall shoppingMall = ShoppingMall();
   shoppingMall.products.addAll([shirt, dress, tShirt, shorts, socks]);
+  bool loop = true;
 
-  print(
-    '''------------------------------------------------------------------------------------------------------
+  while (loop) {
+    print(
+      '''------------------------------------------------------------------------------------------------------
 [1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료
 ------------------------------------------------------------------------------------------------------''',
-  );
+    );
 
-  String? input = stdin.readLineSync();
-  switch (input) {
-    case '1':
-      shoppingMall.showProducts();
-      break;
-    case '2':
-      print('상품 이름을 입력해주세요 !');
-      String? productName = stdin.readLineSync();
-      print('상품 개수를 입력해주세요 !');
+    String? input = stdin.readLineSync();
+    switch (input) {
+      case '1':
+        shoppingMall.showProducts();
+        break;
+      case '2':
+        print('상품 이름을 입력해주세요 !');
+        String? productName = stdin.readLineSync();
+        print('상품 개수를 입력해주세요 !');
 
-      int? productQuantity = int.tryParse(stdin.readLineSync() ?? '');
+        int? productQuantity = int.tryParse(stdin.readLineSync() ?? '');
 
-      if (!shoppingMall.products.any((p) => p.name == productName) ||
-          productQuantity == null) {
-        print('입력값이 올바르지 않아요 !');
-      } else if (productQuantity <= 0) {
-        print('0개보다 많은 개수의 상품만 담을 수 있어요 !');
-      } else {
-        Product product = shoppingMall.products.firstWhere(
-          (p) => p.name == productName,
-        );
-        CartItem item = CartItem(product, productQuantity);
-        shoppingMall.addToCart(item);
-        print('${product.name} ${productQuantity}개가 장바구니에 담겼어요!');
-      }
-      break;
-    case '3':
-    case '4':
+        if (!shoppingMall.products.any((p) => p.name == productName) ||
+            productQuantity == null) {
+          print('입력값이 올바르지 않아요 !');
+        } else if (productQuantity <= 0) {
+          print('0개보다 많은 개수의 상품만 담을 수 있어요 !');
+        } else {
+          Product product = shoppingMall.products.firstWhere(
+            (p) => p.name == productName,
+          );
+          CartItem item = CartItem(product, productQuantity);
+          shoppingMall.addToCart(item);
+          print('장바구니에 상품이 담겼어요 !');
+        }
+        break;
+      case '3':
+        shoppingMall.showTotal();
+        break;
+      case '4':
+        print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
+        loop = false;
+        break;
+    }
   }
 }
