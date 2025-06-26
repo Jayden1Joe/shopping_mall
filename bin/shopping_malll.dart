a2159c84a7eb0cabf8f579d4498b0712cb399831
Jayden1Joe <jojojo69988@naver.com>
2025-06-26 10:31:08 +0900

    Feat(class)

--- bin/shopping_malll.dart
+++ bin/shopping_malll.dart
@@ -1,5 +1,35 @@
 import 'package:shopping_malll/shopping_malll.dart' as shopping_malll;
 
+class ShoppingMall {
+  List<Product> products = [];
+  int total = 0;
+  List<Product> cart = [];
+
+  ShoppingMall();
+
+  showProducts() {
+    for (var product in products) {
+      print('${product.name} / ${product.price}원');
+    }
+  }
+
+  addToCart(Product product) {
+    cart.add(product);
+    total += product.price;
+  }
+
+  showTotal() {
+    print('총 금액: $total원');
+  }
+}
+
+class Product {
+  String name;
+  int price;
+
+  Product(this.name, this.price);
+}
+
 void main(List<String> arguments) {
   print('Hello world: ${shopping_malll.calculate()}!');
 }
