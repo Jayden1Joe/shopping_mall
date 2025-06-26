27683ba2b41ee4ec7c184aafe0f1c7164f48bae4
Jayden1Joe <jojojo69988@naver.com>
2025-06-26 23:02:59 +0900

    Format(add comment)

--- lib/services/shopping_cart.dart
+++ lib/services/shopping_cart.dart
@@ -6,6 +6,7 @@ class ShoppingCart {
   int total = 0;
 
   void addToCart(CartItem item) {
+    // 이미 장바구니에 같은 상품이 있다면 수량을 증가시키고, 없다면 새로 추가
     if (cart.any((cartItem) => cartItem.product.name == item.product.name)) {
       CartItem existingItem = cart.firstWhere(
         (cartItem) => cartItem.product.name == item.product.name,
