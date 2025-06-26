22140de62ce10912406287be9d068f28723f9bb9
Jayden1Joe <jojojo69988@naver.com>
2025-06-26 09:28:10 +0900

    first commit

---
+++ bin/shopping_malll.dart
@@ -0,0 +1,5 @@
+import 'package:shopping_malll/shopping_malll.dart' as shopping_malll;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shopping_malll.calculate()}!');
+}
---
+++ test/shopping_malll_test.dart
@@ -0,0 +1,8 @@
+import 'package:shopping_malll/shopping_malll.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
